30881c6f4ef9fde60b8ea4c085cf6d957e3a20b6
adhicahyadi <cahyadi.adi666@gmail.com>
2024-11-21 14:28:45 +0800

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
 // Ini Javascript
+
+function ValidateForm() {
+    const usernameInput = document.getElementById('username-input').value;
+    console.log(usernameInput);
+    if (usernameInput =='') {
+        alert ('Formnya mohon di isi');
+    } else {
+        alert ('Sukses mengirim form');
+    }
+}
+
+function ValidateForm() {
+    const email = document.getElementById('email').value;
+    console.log(email);
+    if (email =='') {
+        alert ('Formnya mohon di isi');
+    } else {
+        alert ('Sukses mengirim form');
+    }
+}
+
+let indexSlide = 1;
+const listImage = document.getElementsByClassName('banner-autoslide');
+
+showSlide();
+
+function nextSlide () {
+    showSlide(indexSlide += 1)
+}
+
+function showSlide(index) {
+    // Resetter
+    if (index > listImage.length) indexSlide = 1;
+    console.log(indexSlide);
+    hideAllslide();
+    listImage[indexSlide - 1].style.display = 'block';
+}
+
+// Logic untuk menyembunyikan semua slide
+function hideAllslide() {
+    for (let i = 0; i <listImage.length; i++) {
+        listImage[i].style.display = 'none';
+    }
+}
+
+// Otomatisasinya
+setInterval(() => nextSlide(), 2000);
